c73c443ca69013b9d15ce138106a467d24593b8d
Dan Bushong <bushongdan@gmail.com>
2021-01-08 11:09:36 -0600

    numbers and operators show in results

--- logic.js
+++ logic.js
@@ -1,6 +1,44 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Your code here...
+    var operatorClicked = false;
+
+
+
+    $(".number").on("click", function () {
+        if (operatorClicked == false) {
+            $("#first-number").text($("#first-number").text()+$(this).text());
+
+        }
+        else {
+            $("#second-number").text($("#second-number").text()+$(this).text());
+
+        }
+
+    })
+
+
+    // function allOperators(){
+
+    //     $(".equal").on("click", function () {
+
+    //     }
+
+        
+
+
+    // }
+
+
+    $(".operator").on("click", function () {
+    operatorClicked = true;
+    $("#operator").text($(this).text());
+
     
-    
+})
+    $(".clear").on("click", function () {
+    $("#first-number").text($(this).text());
+})
+
+
 });
